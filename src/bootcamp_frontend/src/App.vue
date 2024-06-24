3e48e74aa64bc08294cb0f47fc2a7815ada36aa1
wittor54 <xdwittor@gmail.com>
2024-06-24 20:24:39 +0200

    fix

---
+++ src/bootcamp_frontend/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    <h1>Hello, World!</h1>
+    <h1>{{ num }}</h1>
+    <button @click="add">+</button>
+    <button @click="subtract">-</button>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: 0      
+    }
+  },
+  methods: {
+    add() { this.num++ },
+    subtract() { this.num-- },
+  }
+}
+
+</script>
